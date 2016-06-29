bc8ff17157814c7c4a4bd05888077ab8e2efa5ed
Anders Sundman <anders@4zm.org>
2016-06-29 08:56:59 +0200

    Moved training info page to training dir

--- app/pages/start-page/start-page.ts
+++ app/pages/start-page/start-page.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
-import {TrainingIntroPage} from '../training-intro/training-intro';
+import {TrainingIntroPage} from '../training/intro';
 import {IatPage} from '../iat/iat';
 import {SettingsPage} from '../settings/settings';
 
