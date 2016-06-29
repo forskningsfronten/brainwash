0a2b77a626dbd0e479465d0b42e7878de4eb9780
Anders Sundman <anders@4zm.org>
2016-06-30 00:04:50 +0200

    Setting to skip training instructions

--- app/pages/start-page/start-page.ts
+++ app/pages/start-page/start-page.ts
@@ -1,20 +1,30 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {TrainingIntroPage} from '../training/intro';
+import {TrainingPage} from '../training/training';
 import {IatPage} from '../iat/iat';
 import {SettingsPage} from '../settings/settings';
+import {SettingsStorage} from '../settings/settings-storage';
+
 
 @Component({
   templateUrl: 'build/pages/start-page/start-page.html'
 })
-
 export class StartPage {
+  private settingsStorage = new SettingsStorage();
+
   constructor(private nav: NavController, navParams: NavParams) {
-    //this.openSettings();
   }
 
   startTraining() {
-    this.nav.push(TrainingIntroPage);
+    this.settingsStorage
+      .getValue(this.settingsStorage.showTrainingInstructionsKey)
+      .then(val => {
+        if (val === 'false')
+          this.nav.push(TrainingPage);
+        else
+          this.nav.push(TrainingIntroPage);
+      });
   }
 
   startIAT() {
